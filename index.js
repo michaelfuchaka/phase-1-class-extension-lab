04a95edf90ac4f3baca87b2c56b2253d7c58bff6
michaelfuchaka <fuchakamichael06@gmail.com>
2025-06-29 22:46:06 +0300

    Add Polygon, Triangle, and Square classes with inheritance and computed properties

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+// Your code here// Base class: Polygon
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((sum, side) => sum + side, 0);
+  }
+}
+
+// Triangle class extends Polygon
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.countSides !== 3) return false;
+    const [a, b, c] = this.sides;
+    return (a + b > c) && (a + c > b) && (b + c > a);
+  }
+}
+
+// Square class extends Polygon
+class Square extends Polygon {
+  get isValid() {
+    if (this.countSides !== 4) return false;
+    return this.sides.every(side => side === this.sides[0]);
+  }
+
+  get area() {
+    return this.isValid ? this.sides[0] ** 2 : null;
+  }
+}
